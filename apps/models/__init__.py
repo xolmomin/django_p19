f21f3cb2694aa52d82ca93207465a87e3088634f
xolmomin <xolmomin@gmail.com>
2024-05-29 18:09:29 +0500

    change

---
+++ apps/models/__init__.py
@@ -0,0 +1,3 @@
+from apps.models.course import Course, Category
+from apps.models.user import User
+from apps.models.proxies import TeacherUserProxy, ModeratorUserProxy, AdminUserProxy, StudentUserProxy
